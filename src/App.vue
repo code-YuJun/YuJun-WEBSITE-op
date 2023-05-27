a292310335424462dfc3206c0ce0757253f7d3b1
YuJun <2594131385@qq.com>
2023-05-27 16:08:25 +0800

    feat: 引入 svg 图标

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <el-button type="primary">Primary</el-button>
+    <!-- <svg :style="{ width: '10px', height: '10px' }">
+      <use xlink:href="#icon-test"></use>
+    </svg> -->
+    <svg-icon name="test" width="600px" height="300px"></svg-icon>
   </div>
 </template>
 
 <script>
 export default {
   setup() {
-    console.log("显示");
-    console.log(import.meta.env.VITE_APP_BASE_API);
     return {};
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import App from "@/App.vue";
 // 引入组件库
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+//svg插件需要配置代码
+import 'virtual:svg-icons-register';
+//引入自定义插件对象:注册整个项目全局组件
+import gloalComponent from '@/components';
 //@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
 import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 
@@ -11,4 +15,6 @@ const app = createApp(App);
 app.use(ElementPlus, {
   locale: zhCn,
 });
+//安装自定义插件
+app.use(gloalComponent);
 app.mount("#app");
