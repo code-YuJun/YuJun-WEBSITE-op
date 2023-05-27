d9b06983a7e503f644f82c0a7328cd855b0b3b78
YuJun <2594131385@qq.com>
2023-05-27 17:18:34 +0800

    feat: 分模块请求

--- utils/request.ts
+++ utils/request.ts
@@ -2,26 +2,28 @@ import axios from "axios";
 import { ElMessage } from "element-plus";
 //创建axios实例
 let request = axios.create({
-    baseURL: import.meta.env.VITE_APP_BASE_API,
-    timeout: 5000
-  })
-  //请求拦截器
-request.interceptors.request.use(config => {
-    return config;
-  });
-  //响应拦截器
-request.interceptors.response.use((response) => {
+  baseURL: import.meta.env.VITE_APP_BASE_API,
+  timeout: 5000,
+});
+//请求拦截器
+request.interceptors.request.use((config) => {
+  return config;
+});
+//响应拦截器
+request.interceptors.response.use(
+  (response) => {
     return response.data;
-  }, (error) => {
+  },
+  (error) => {
     //处理网络错误
-    let msg = '';
+    let msg = "";
     let status = error.response.status;
     switch (status) {
       case 401:
         msg = "token过期";
         break;
       case 403:
-        msg = '无权访问';
+        msg = "无权访问";
         break;
       case 404:
         msg = "请求地址错误";
@@ -33,9 +35,10 @@ request.interceptors.response.use((response) => {
         msg = "无网络";
     }
     ElMessage({
-      type: 'error',
-      message: msg
-    })
+      type: "error",
+      message: msg,
+    });
     return Promise.reject(error);
-  });
-  export default request;
+  }
+);
+export default request;
