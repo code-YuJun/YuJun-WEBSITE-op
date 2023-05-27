bace424314d3175d9e3b2f9b09899bf2366f3d02
YuJun <2594131385@qq.com>
2023-05-27 17:18:02 +0800

    feat: 分模块请求

---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+import request from '@/utils/request'
+
+// 请求参数
+import type {
+    loginFormData
+} from './type'
+
+//项目用户相关的请求地址
+enum API {
+    LOGIN_URL = '/admin/acl/index/login'
+}
+
+//登录接口
+export const reqLogin = (data: loginFormData) => request.post(API.LOGIN_URL, data)
